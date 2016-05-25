499a960f59a41b062c5a8affbb90b3d53817ec65
GaneshCloud <rgkcloudmail@gmail.com>
2016-05-25 11:19:53 +0530

    Setup guide to run project.

--- public/app/core/config.js
+++ public/app/core/config.js
@@ -5,15 +5,15 @@ app.config(function($routeProvider, $locationProvider){
 	$routeProvider.
 		when("/project/home",{
 			controller: "homeController",
-			templateUrl: "../app/Home/home.html"
+			templateUrl: "../app/project/home/home.html"
 		}).
 		// when("/", {
 		// 	redirectTo:"/project/home"
 		// }).
-		when("/upload", {
+		when("/project/upload", {
 			controller: "uploadController",
 			controllerAs: "up",
-			templateUrl: "../app/Upload/upload.html"		
+			templateUrl: "../app/project/upload/upload.html"
 		}).
 		when("/error",{
 			templateUrl: "../app/Error/error.html"		
