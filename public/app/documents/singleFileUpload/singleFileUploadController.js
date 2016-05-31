83a7c0dfd5d131bf41cb6ac7538be6b7e42582f6
januNithi <csejananim@gmail.com>
2016-05-31 16:22:25 +0530

    Updated with code style changes

--- public/app/documents/singleFileUpload/singleFileUploadController.js
+++ public/app/documents/singleFileUpload/singleFileUploadController.js
@@ -1,127 +1,125 @@
 
-'use strict';
 myApp.controller('singleFileUploadController', function($scope,uploadSingleServices,adminDashboardService,$window) {
-  
-        //document.getElementById("docCaption").focus();
-        $scope.formData = {
-          ID:"",
-          DOCCAPTION:"",
-          DOCTYPE:"",
-          DOCDEP:"",
-          DOCKEY:"",
-          DOCDESC:"",
-          DOCFILE:""
-        };                      //model object for store the document input data
 
-        $scope.docs={};         
-        $scope.isErr=false;     //model for checking error
-        $scope.isNull=true;     //mode for checks the null value;
-        $scope.docPattern='';   //model for pattern
-        $scope.search={};       
-        $scope.dep=[];          //model for store the department details
-        $scope.type=[
-                     {id:1,type:'PDF Document',ptrn:".pdf"},
-                     {id:2,type:'Word Document',ptrn:".docx"},
-                     {id:3,type:'Slide Document',ptrn:".ppt"},
-                     {id:4,type:'Image Document',ptrn:"image/*"},
-                     {id:5,type:'Archive Document',ptrn:"*.zip|*.rar"},
-                     {id:6,type:'Video Document',ptrn:".mp4"}
+  //Document.getElementById("docCaption").focus();
+  $scope.formData = {
+          ID: '',
+          DOCCAPTION: '',
+          DOCTYPE: '',
+          DOCDEP: '',
+          DOCKEY: '',
+          DOCDESC: '',
+          DOCFILE: ''
+        };                      //Model object for store the document input data
 
-                    ];          //model for document type
-        adminDashboardService.checkAdmin();
-       //function for get the pattern details 
-       $scope.getPattern=function(ptrn){ 
-           $scope.docPattern=$scope.type[ptrn-1].ptrn; 
-       };
+  $scope.docs = {};
+  $scope.isErr = false;     //Model for checking error
+  $scope.isNull = true;     //Mode for checks the null value;
+  $scope.docPattern = '';   //Model for pattern
+  $scope.search = {};
+  $scope.dep = [];          //Model for store the department details
+  $scope.type = [
+               {id: 1,type: 'PDF Document',ptrn: '.pdf'},
+               {id: 2,type: 'Word Document',ptrn: '.docx'},
+               {id: 3,type: 'Slide Document',ptrn: '.ppt'},
+               {id: 4,type: 'Image Document',ptrn: 'image/*'},
+               {id: 5,type: 'Archive Document',ptrn: '*.zip|*.rar'},
+               {id: 6,type: 'Video Document',ptrn: '.mp4'}
 
-       //function for save the document
-       $scope.saveDoc = function() {
-                    alert('Saved Successfully');
-                    document.forms.frmDoc.submit();      
-        };
+              ];          //Model for document type
+  adminDashboardService.checkAdmin();
+  //Function for get the pattern details
+  $scope.getPattern = function(ptrn) {
+    $scope.docPattern = $scope.type[ptrn - 1].ptrn;
+  };
 
-        //function for liast the document api
-        $scope.documentList = function() {
-            uploadSingleServices.documentList();
-        };
+  //Function for save the document
+  $scope.saveDoc = function() {
+    alert('Saved Successfully');
+    document.forms.frmDoc.submit();
+  };
 
-        $scope.multipleFileUpload=function(){
-            uploadSingleServices.multipleFileUpload();
-        };
+  //Function for liast the document api
+  $scope.documentList = function() {
+    uploadSingleServices.documentList();
+  };
 
-        //function for setting the data for editting
-        $scope.editForm = function() {
+  $scope.multipleFileUpload = function() {
+    uploadSingleServices.multipleFileUpload();
+  };
 
-            
-                
-                uploadSingleServices.edit($scope.getParameterByName("id"))
+  //Function for setting the data for editting
+  $scope.editForm = function() {
+
+
+
+    uploadSingleServices.edit($scope.getParameterByName('id'))
                     .success(function(data) {
 
-                       $scope.formData = data[0];
-                       $scope.getPattern(data[0].DOCTYPE);
+                      $scope.formData = data[0];
+                      $scope.getPattern(data[0].DOCTYPE);
                     })
-                    .error(function(err){
-                       console.log(err);
+                    .error(function(err) {
+                      console.log(err);
                     });
-               
-        };
 
-        //function for getting the parameter value by url
-        $scope.getParameterByName=function (name, url) {
+  };
+
+  //Function for getting the parameter value by url
+  $scope.getParameterByName = function(name, url) {
           if (!url) url = window.location.href;
-              name = name.replace(/[\[\]]/g, "\\$&");
-              var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
-              results = regex.exec(url);
-              if (!results) return null;
-              if (!results[2]) return '';
-              return decodeURIComponent(results[2].replace(/\+/g, " "));
+          name = name.replace(/[\[\]]/g, '\\$&');
+          var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)', 'i'),
+          results = regex.exec(url);
+          if (!results) return null;
+          if (!results[2]) return '';
+          return decodeURIComponent(results[2].replace(/\+/g, ' '));
         };
-         
 
-        //function for get the department details
-        $scope.getDepartment=function(){
-            uploadSingleServices.getDepartment()
-            .success(function(data){
-                $scope.dep=data;
+
+  //Function for get the department details
+  $scope.getDepartment = function() {
+    uploadSingleServices.getDepartment()
+            .success(function(data) {
+              $scope.dep = data;
             })
-            .error(function(err){
+            .error(function(err) {
               console.log(err);
             });
-        };
-       
+  };
 
 
-        //initially calling the functions for page loading
-        $scope.getDepartment();
-        
-       
-        if(window.location.pathname=="/documents/editDoc")
-            $scope.editForm();
 
-        $scope.$on("fileSelected", function (event, args) {
-            $scope.$apply(function () {            
-             $scope.files.push(args.file);
-          });
-        });
-
-        // lodout
-        $scope.onLogout = function(){
-
-        if ($window.confirm("Are You Sure ! Do you need to Log Out?")) {
-
-            uploadSingleServices.logout();
-
-        }
-
-        };
-        //Dashboard
-        $scope.goToDashboard = function(){
-
-            uploadSingleServices.goToDashboard();
-
-        };
+  //Initially calling the functions for page loading
+  $scope.getDepartment();
 
 
+  if (window.location.pathname == '/documents/editDoc')
+$scope.editForm();
+
+  $scope.$on('fileSelected', function(event, args) {
+    $scope.$apply(function() {
+      $scope.files.push(args.file);
     });
-      
- 
+  });
+
+  // Lodout
+  $scope.onLogout = function() {
+
+    if ($window.confirm('Are You Sure ! Do you need to Log Out?')) {
+
+      uploadSingleServices.logout();
+
+    }
+
+  };
+  //Dashboard
+  $scope.goToDashboard = function() {
+
+    uploadSingleServices.goToDashboard();
+
+  };
+
+
+});
+
