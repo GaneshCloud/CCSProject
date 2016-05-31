91f8701ced5fd4b8fa42d28437ab0227a186265c
januNithi <csejananim@gmail.com>
2016-05-31 16:19:00 +0530

    Updated with code style changes

--- public/app/documents/services/starServices.js
+++ public/app/documents/services/starServices.js
@@ -1,19 +1,17 @@
-'use strict';
-
 myApp.factory('starServices', function($http) {
-   
-   return{
-            setStar:function(data){
-                return $http.post('/api/setStar',data);
-            },
-            getStar:function(data){
-                return $http.get('/api/getStar'+data);
-            },
-            getStarInfo:function(id){
-                return $http.get('/getRatingInfo?id='+id);
-            }
+
+  return {
+    setStar: function(data) {
+      return $http.post('/api/setStar',data);
+    },
+    getStar: function(data) {
+      return $http.get('/api/getStar' + data);
+    },
+    getStarInfo: function(id) {
+      return $http.get('/getRatingInfo?id=' + id);
+    }
 
 
-        };
-            
+  };
+
 });
