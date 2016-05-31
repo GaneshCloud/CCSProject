75ad789e942983c974437adae2c6ed2a0162bf9b
januNithi <csejananim@gmail.com>
2016-05-31 16:58:03 +0530

    Updated with code style changes

--- public/app/profile/userProfile/userProfileService.js
+++ public/app/profile/userProfile/userProfileService.js
@@ -2,86 +2,86 @@
  * Created by CSS on 25-05-2016.
  */
 angular.module('myApp')
-    .factory('userProfileService', function($http, $window, $q, Upload, $timeout) {
-        var httpPromise;
-        return {
-            changePassword: function () {
-                $window.location.href = '/profile/changePassword';
-            },
+    .factory('userProfileService', function($http, $window, $q, Upload) {
+      var httpPromise;
+      return {
+        changePassword: function() {
+          $window.location.href = '/profile/changePassword';
+        },
 
-            pageReload: function () {
-                $window.location.reload();
-            },
+        pageReload: function() {
+          $window.location.reload();
+        },
 
-            goToDashboard:function () {
-                $window.location.href = '/profile/userDashboard';
-            },
+        goToDashboard: function() {
+          $window.location.href = '/profile/userDashboard';
+        },
 
-            addFacebookAccount: function () {
-                $window.location.href = '/connect/facebook';
-            },
+        addFacebookAccount: function() {
+          $window.location.href = '/connect/facebook';
+        },
 
-            addGoogleAccount: function () {
-                $window.location.href = '/connect/google';
-            },
+        addGoogleAccount: function() {
+          $window.location.href = '/connect/google';
+        },
 
-            logout: function () {
-                $window.location.href = '/logout';
-            },
+        logout: function() {
+          $window.location.href = '/logout';
+        },
 
-            getPersonalData: function () {
-                var deferred = $q.defer();
+        getPersonalData: function() {
+          var deferred = $q.defer();
 
-                httpPromise = $http.get('/connect/getPersonalData');
+          httpPromise = $http.get('/connect/getPersonalData');
 
-                httpPromise.then(function (response) {
-                    deferred.resolve(response);
-                }, function (error) {
-                    console.error(error);
+          httpPromise.then(function(response) {
+            deferred.resolve(response);
+          }, function(error) {
+            console.error(error);
 
-                    $window.location.href = '/error';
-                });
+            $window.location.href = '/error';
+          });
 
-                return deferred.promise;
-            },
+          return deferred.promise;
+        },
 
-            uploadImage: function (file) {
-                var dfr = $q.defer();
+        uploadImage: function(file) {
+          var dfr = $q.defer();
 
-                httpPromise = Upload.upload({
-                    url: '/uploadImage',
+          httpPromise = Upload.upload({
+            url: '/uploadImage',
 
-                    data: {file: file}
+            data: {file: file}
 
-                });
+          });
 
-                httpPromise.then(function (response) {
-                    dfr.resolve(response);
-                }, function (error) {
-                    console.error(error);
-                });
-                return dfr.promise;
-            },
+          httpPromise.then(function(response) {
+            dfr.resolve(response);
+          }, function(error) {
+            console.error(error);
+          });
+          return dfr.promise;
+        },
 
-            updatePersonalData: function (personalData) {
+        updatePersonalData: function(personalData) {
 
-                var dfr = $q.defer();
+          var dfr = $q.defer();
 
-                httpPromise = $http({
-                    method: 'post',
+          httpPromise = $http({
+            method: 'post',
 
-                    url: '/connect/updatePersonalData',
+            url: '/connect/updatePersonalData',
 
-                    data: personalData
+            data: personalData
 
-                });
-                httpPromise.then(function (response) {
-                    dfr.resolve(response);
-                }, function (error) {
-                    console.error(error);
-                });
-                return dfr.promise;
-            }
+          });
+          httpPromise.then(function(response) {
+            dfr.resolve(response);
+          }, function(error) {
+            console.error(error);
+          });
+          return dfr.promise;
+        }
 
-        };
+      };
     });
