baf791cdcb039057523bad9d58db317faca21c24
januNithi <csejananim@gmail.com>
2016-05-31 17:52:50 +0530

    Updated with code style changes

--- config/db/documents/documentdb.js
+++ config/db/documents/documentdb.js
@@ -1,340 +1,297 @@
-module.exports = function(con){
+module.exports = function(con) {
 
-//var techDocument=require("../models/document");
+  //Var techDocument=require("../models/document");
 
 
-this.insertDoc=function(data,cb){
+  this.insertDoc = function(data,cb) {
 
-      var myErr=null,insertId=null;
-      var qry="INSERT INTO TBL_DOCUMENTS SET ? ";
+    var myErr = null,insertId = null;
+    var qry = 'INSERT INTO TBL_DOCUMENTS SET ? ';
 
-      con.query(qry,data, function(err,res){
+    con.query(qry,data, function(err,res) {
 
-      if(err)
-      {
-      	  myErr=err;
-      	  cb( myErr,insertId);
-      	}
-      else
-      {
-        	insertId=res.insertId;
-         	cb(myErr,res.insertId);
+      if (err) {
+        myErr = err;
+        cb(myErr,insertId);
+      	} else {
+        	insertId = res.insertId;
+        cb(myErr,res.insertId);
       }
-      });
-};
+    });
+  };
 
 
 
-this.getAllDoc=function(ser,cb){
+  this.getAllDoc = function(ser,cb) {
 
-      var myErr=null,data=null;
-      var qry="SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCCAPTION LIKE '" + ser+ "%' GROUP BY A.id";
-      console.log(qry);
-      con.query(qry,function(err,res){
+    var myErr = null,data = null;
+    var qry = 'SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCCAPTION LIKE \'' + ser + '%\' GROUP BY A.id';
+    console.log(qry);
+    con.query(qry,function(err,res) {
 
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        console.log(res);
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          console.log(res);
-          data=res;
-          cb(myErr,data);
-       }
-   });
-};
+    });
+  };
 
-this.updateDoc=function(id,uData,cb){
+  this.updateDoc = function(id,uData,cb) {
 
-      var myErr=null,data=null;
-      var qry="UPDATE TBL_DOCUMENTS SET ? WHERE ID="+id;
-      con.query(qry,uData,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
+    var myErr = null,data = null;
+    var qry = 'UPDATE TBL_DOCUMENTS SET ? WHERE ID=' + id;
+    con.query(qry,uData,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
-};
+    });
+  };
 
-this.deleteDoc=function(id,cb){
+  this.deleteDoc = function(id,cb) {
 
-     var myErr=null,data=null;
-     var objectConstructor = {}.constructor;
+    var myErr = null,data = null;
+    var objectConstructor = {}.constructor;
 
-     if(id==""||id==null) return false;
-     if(id.constructor!==objectConstructor)
-      {
-          cb("not an object",data);
-          return;
+    if (id === '' || id === null) return false;
+    if (id.constructor !== objectConstructor)     {
+      cb('not an object',data);
+      return;
+    }
+
+    var qry = 'DELETE  FROM TBL_DOCUMENTS WHERE ?';
+    con.query(qry,id,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
+    });
+  };
 
-     var qry="DELETE  FROM TBL_DOCUMENTS WHERE ?";
-	   con.query(qry,id,function(err,res){
-  		
-      if(err)
-      { 
-          myErr=err;
-          cb(myErr,data);
+  this.getDocById = function(id,cb) {
+
+    var myErr = null,data = null;
+    var qry = 'SELECT  * FROM TBL_DOCUMENTS WHERE ID=' + id;
+
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
-};
+    });
 
-this.getDocById=function(id,cb){
+  };
 
-      var myErr=null,data=null;
-      var qry="SELECT  * FROM TBL_DOCUMENTS WHERE ID="+id;
-     
-      con.query(qry,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
+  this.getDocByDep = function(ser,depId,cb) {
+
+    var myErr = null,data = null;
+    var qry = 'SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCDEP=' + depId + ' AND A.DOCCAPTION LIKE \'' + ser + '%\' GROUP BY C.doc_id,A.ID';
+    con.query(qry,depId,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
+    });
 
-};
+  };
 
-this.getDocByDep=function(ser,depId,cb){
+  this.getDocByType = function(ser,typeId,cb) {
 
-      var myErr=null,data=null;
-      var qry="SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCDEP="+depId+" AND A.DOCCAPTION LIKE '" + ser+ "%' GROUP BY C.doc_id,A.ID";
-      con.query(qry,depId,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
+    var myErr = null,data = null;
+    var qry = 'SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCTYPE=' + typeId + ' AND A.DOCCAPTION LIKE \'' + ser + '%\' GROUP BY C.doc_id,A.ID';
+    con.query(qry,typeId,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
+    });
 
-};
+  };
 
-this.getDocByType=function(ser,typeId,cb){
+  this.getDocByTypeDep = function(ser,typeId,depId,cb) {
 
-      var myErr=null,data=null;
-      var qry="SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCTYPE="+ typeId+" AND A.DOCCAPTION LIKE '" + ser+ "%' GROUP BY C.doc_id,A.ID";
-      con.query(qry,typeId,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
+    var myErr = null,data = null;
+    var qry = 'SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCTYPE=' + typeId + ' AND A.DOCDEP=' + depId + ' AND A.DOCCAPTION LIKE \'' + ser + '%\' GROUP BY C.doc_id,A.ID';
+    console.log(qry);
+    con.query(qry,typeId,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,data);
+      } else {
+        data = res;
+        cb(myErr,data);
       }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
+    });
 
-};
+  };
 
-this.getDocByTypeDep=function(ser,typeId,depId,cb){
+  this.incrViews = function(id,cb) {
 
-      var myErr=null,data=null;
-      var qry="SELECT A.*,B.*,COALESCE(FLOOR(AVG(C.STARS)),0) as star FROM TBL_DOCUMENTS A LEFT OUTER join TBL_DEPARTMENT B on (A.DOCDEP=B.DEP_ID) LEFT OUTER JOIN  TBL_DOC_STARS C  on  A.ID=C.DOC_ID WHERE A.DOCTYPE="+typeId+ " AND A.DOCDEP="+depId+" AND A.DOCCAPTION LIKE '" + ser+ "%' GROUP BY C.doc_id,A.ID";
-      console.log(qry);
-      con.query(qry,typeId,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,data);
-      }
-      else
-      {
-          data=res;
-          cb(myErr,data);
-       }
-   });
-
-};
-
-this.incrViews=function(id,cb){
-    
-      var qry='UPDATE TBL_DOCUMENTS SET DOCNOVIEWS = DOCNOVIEWS + 1 WHERE id ='+id;
-      con.query(qry,function(err,res){
+    var qry = 'UPDATE TBL_DOCUMENTS SET DOCNOVIEWS = DOCNOVIEWS + 1 WHERE id =' + id;
+    con.query(qry,function(err,res) {
       cb(err,JSON.stringify(res));
-      
-   });
-};
 
-this.incrDown=function(id,cb){
+    });
+  };
 
-      var qry='UPDATE TBL_DOCUMENTS SET DOCNODOWN = DOCNODOWN + 1 WHERE id ='+id;
-      con.query(qry,function(err,res){
+  this.incrDown = function(id,cb) {
+
+    var qry = 'UPDATE TBL_DOCUMENTS SET DOCNODOWN = DOCNODOWN + 1 WHERE id =' + id;
+    con.query(qry,function(err,res) {
 
       cb(err,JSON.stringify(res));
 
     });
-};
+  };
 
 
-this.getLatestDoc=function(depId,cb){
+  this.getLatestDoc = function(depId,cb) {
 
-      var myErr=null,result=null;
-      var qry='select * from tbl_documents where DOCDEP='+depId+' ORDER BY ID  DESC LIMIT 5';
+    var myErr = null,result = null;
+    var qry = 'select * from tbl_documents where DOCDEP=' + depId + ' ORDER BY ID  DESC LIMIT 5';
 
-      con.query(qry,function(err,res){
+    con.query(qry,function(err,res) {
 
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
       }
-      else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-
-};
-
-this.getLatestVid=function(id,cb){
-
-      var myErr=null,result=null;
-      var qry='select * from tbl_documents where doctype='+id+' ORDER BY ID  DESC LIMIT 2';
-
-      con.query(qry,function(err,res){
-
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
-      }
-      else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-
-};
-
-this.getLatestImg=function(id,cb){
-
-      var myErr=null,result=null;
-      var qry='select a.*,b.*,substring_index(a.docfile,".",-1) as type from tbl_documents a inner join tbl_department b on a.docdep=b.dep_id where doctype='+id+' ORDER BY ID  DESC LIMIT 4';
-      con.query(qry,function(err,res){
-  		
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
-      }
-      else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-
-};
-
-
-this.getLatestArchives=function(id,cb){
-
-      var myErr=null,result=null;
-      var qry='select a.*,b.*,substring_index(a.docfile,".",-1) as type from tbl_documents a inner join tbl_department b on a.docdep=b.dep_id where doctype='+id+' ORDER BY ID  DESC LIMIT 4';
-      
-      con.query(qry,function(err,res){
-      
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
-      }
-      else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-};
-
-
-this.getNextDocument=function(id,cb){
-
-      var myErr=null,result=null;
-      var qry='select * from TBL_DOCUMENTS where id = (select min(id) from TBL_DOCUMENTS where id > '+ id+' )';
-
-      con.query(qry,function(err,res){
-     
-      if(err) throw err;
-
-      else
-          console.log(res);
-
-      });
-   
-   con.query(qry,function(err,res){
-      
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
-      }
-      else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-};
-
-
-this.getPrevDocument=function(id,cb){
-
-      var myErr=null,result=null;
-      var qry='select * from TBL_DOCUMENTS where id = (select max(id) from TBL_DOCUMENTS where id < '+ id+' )';
-
-      con.query(qry,function(err,res){
-
-      if(err) throw err;
-      else
-          console.log(res);
-     
     });
 
-      con.query(qry,function(err,res){
-      
-      if(err)
-      {
-          myErr=err;
-          cb(myErr,result);
+  };
+
+  this.getLatestVid = function(id,cb) {
+
+    var myErr = null,result = null;
+    var qry = 'select * from tbl_documents where doctype=' + id + ' ORDER BY ID  DESC LIMIT 2';
+
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
       }
+    });
+
+  };
+
+  this.getLatestImg = function(id,cb) {
+
+    var myErr = null,result = null;
+    var qry = 'select a.*,b.*,substring_index(a.docfile,".",-1) as type from tbl_documents a inner join tbl_department b on a.docdep=b.dep_id where doctype=' + id + ' ORDER BY ID  DESC LIMIT 4';
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
+      }
+    });
+
+  };
+
+
+  this.getLatestArchives = function(id,cb) {
+
+    var myErr = null,result = null;
+    var qry = 'select a.*,b.*,substring_index(a.docfile,".",-1) as type from tbl_documents a inner join tbl_department b on a.docdep=b.dep_id where doctype=' + id + ' ORDER BY ID  DESC LIMIT 4';
+
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
+      }
+    });
+  };
+
+
+  this.getNextDocument = function(id,cb) {
+
+    var myErr = null,result = null;
+    var qry = 'select * from TBL_DOCUMENTS where id = (select min(id) from TBL_DOCUMENTS where id > ' + id + ' )';
+
+    con.query(qry,function(err,res) {
+
+      if (err) throw err;
+
       else
-      {
-          result=res;
-          cb(myErr,result);
-       }
-   });
-};
+          console.log(res);
+
+    });
+
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
+      }
+    });
+  };
+
+
+  this.getPrevDocument = function(id,cb) {
+
+    var myErr = null,result = null;
+    var qry = 'select * from TBL_DOCUMENTS where id = (select max(id) from TBL_DOCUMENTS where id < ' + id + ' )';
+
+    con.query(qry,function(err,res) {
+
+      if (err) throw err;
+      else
+          console.log(res);
+
+    });
+
+    con.query(qry,function(err,res) {
+
+      if (err) {
+        myErr = err;
+        cb(myErr,result);
+      } else {
+        result = res;
+        cb(myErr,result);
+      }
+    });
+  };
 
 };
--- config/db/forumManager.js
+++ config/db/forumManager.js
@@ -9,139 +9,139 @@ var ConfigManager = require('./connectionManager');
 
 function getForum(type) {
 
-    var deferred = q.defer();
-    var questions = [];
-    var getQus = '';
+  var deferred = q.defer();
+  var questions = [];
+  var getQus = '';
 
-    if (type == 'All') {
-        getQus = 'select q.*, (select round(AVG(rating)) from rating where qusId=q.qusId) as rating from question as q order by qusId DESC';
-        console.log(getQus);
-    } else {
-        getQus = 'select * from question where Type=\'' + type + '\' order by qusId DESC ';
-    }
-    ConfigManager.getConnection()
+  if (type === 'All') {
+    getQus = 'select q.*, (select round(AVG(rating)) from rating where qusId=q.qusId) as rating from question as q order by qusId DESC';
+    console.log(getQus);
+  } else {
+    getQus = 'select * from question where Type=\'' + type + '\' order by qusId DESC ';
+  }
+  ConfigManager.getConnection()
         .then(function(connection) {
-            connection.query(getQus, function(err, result) {
+          connection.query(getQus, function(err, result) {
 
-                if (err) {
+            if (err) {
+              deferred.reject(err);
+              throw err;
+            }
+
+            if (result) {
+              data = result;
+
+              console.log(JSON.stringify(data[0]));
+
+              console.log(JSON.stringify(data.length));
+
+              data.forEach(function(value, index) {
+                console.log(value);
+
+                var get = 'select * from answer where qusId=' + value.qusId + ' order by id DESC';
+
+                console.log('get answer query' + get);
+
+                connection.query(get, function(err, results) {
+                  if (err) {
                     deferred.reject(err);
                     throw err;
-                }
+                  }
 
-                if (result) {
-                    data = result;
+                  console.log(results);
+                  if (results) {
+                    value.answers = results;
 
-                    console.log(JSON.stringify(data[0]));
+                    console.log('value' + JSON.stringify(value));
 
-                    console.log(JSON.stringify(data.length));
+                    questions.push(value);
 
-                    data.forEach(function(value, index) {
-                        console.log(value);
+                    if ((index + 1) >= data.length) {
+                      deferred.resolve(questions);
+                    }
 
-                        var get = 'select * from answer where qusId=' + value.qusId + ' order by id DESC';
-
-                        console.log('get answer query' + get);
-
-                        connection.query(get, function(err, results) {
-                            if (err) {
-                                deferred.reject(err);
-                                throw err;
-                            }
-
-                            console.log(results);
-                            if (results) {
-                                value.answers = results;
-
-                                console.log('value' + JSON.stringify(value));
-
-                                questions.push(value);
-
-                                if ((index + 1) >= data.length) {
-                                    deferred.resolve(questions);
-                                }
-
-                            }
-                        });
-                    });
-                }
-            });
+                  }
+                });
+              });
+            }
+          });
         });
-    return deferred.promise;
+  return deferred.promise;
 
 }
 
 function postForumquestion(forumData,userid) {
-    var deferred = q.defer();
-    ConfigManager.getConnection()
+  var deferred = q.defer();
+  ConfigManager.getConnection()
         .then(function(connection) {
 
-            connection.query('INSERT INTO question(Question,Dates,Type,Explation,userid) values(\'' + forumData.question + '\',now(),\'' + forumData.Type + '\',\'' + forumData.explation + '\',\'' + userid + '\')', function(err, result) {
-                if (err) {
-                    console.log(err);
-                    deferred.reject(err);
-                } else {
-                    console.log('Successfully');
-                    deferred.resolve(result);
-                }
-            });
+          connection.query('INSERT INTO question(Question,Dates,Type,Explation,userid) values(\'' + forumData.question + '\',now(),\'' + forumData.Type + '\',\'' + forumData.explation + '\',\'' + userid + '\')', function(err, result) {
+            if (err) {
+              console.log(err);
+              deferred.reject(err);
+            } else {
+              console.log('Successfully');
+              deferred.resolve(result);
+            }
+          });
         });
 
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function postForumAnswer(forumData) {
-    var deferred = q.defer();
-    ConfigManager.getConnection()
+  var deferred = q.defer();
+  ConfigManager.getConnection()
         .then(function(connection) {
 
-            var qry = 'INSERT INTO answer(qusId,Date,Answers)values(' + forumData.qusId + ',now(),\'' + forumData.Comment + '\')';
+          var qry = 'INSERT INTO answer(qusId,Date,Answers)values(' + forumData.qusId + ',now(),\'' + forumData.Comment + '\')';
 
-            console.log('Post Answer Query --->' + qry);
+          console.log('Post Answer Query --->' + qry);
 
-            connection.query(qry, function(err, result) {
-                if (err) {
-                    console.log('Error when get postAnswer data : ' + err);
-                    deferred.reject(err);
-                    return;
-                } else {
-                    console.log(result);
-                    deferred.resolve(result);
-                }
-            });
+          connection.query(qry, function(err, result) {
+            if (err) {
+              console.log('Error when get postAnswer data : ' + err);
+              deferred.reject(err);
+              return;
+            } else {
+              console.log(result);
+              deferred.resolve(result);
+            }
+          });
         })
         .fail(function(err) {
-            console.error(JSON.stringify(err));
-            deferred.reject(err);
+          console.error(JSON.stringify(err));
+          deferred.reject(err);
         });
-    return deferred.promise;
+  return deferred.promise;
 }
 
 function postForumRating(ratingArr) {
-    var deferred = q.defer();
-    ConfigManager.getConnection()
+  var deferred = q.defer();
+  ConfigManager.getConnection()
         .then(function(connection) {
 
-            connection.query('insert into Rating(qusId,rating) values(' + ratingArr[0] + ',' + ratingArr[1] + ')', function(err, result) {
-                console.log('insert into Rating(qusId,rating) values(' + ratingArr[0] + ',' + ratingArr[1] + ')');
-                if (err) {
-                    console.log(err);
-                    deferred.reject(err);
-                } else {
-                    console.log('Successfully');
-                    deferred.resolve({affectedRows: result.affectedRows});
-                }
-            });
+          connection.query('insert into Rating(qusId,rating) values(' + ratingArr[0] + ',' + ratingArr[1] + ')', function(err, result) {
+            console.log('insert into Rating(qusId,rating) values(' + ratingArr[0] + ',' + ratingArr[1] + ')');
+            if (err) {
+              console.log(err);
+              deferred.reject(err);
+            } else {
+              console.log('Successfully');
+              deferred.resolve({affectedRows: result.affectedRows});
+            }
+          });
         })
         .fail(function(err) {
-            console.error(JSON.stringify(err));
-            deferred.reject(err);
+          console.error(JSON.stringify(err));
+          deferred.reject(err);
         });
-    return deferred.promise;
+  return deferred.promise;
 }
 
 module.exports = {
-    getForum: getForum,
-    postForumquestion: postForumquestion,
-    postForumAnswer: postForumAnswer,
-    postForumRating: postForumRating,
+  getForum: getForum,
+  postForumquestion: postForumquestion,
+  postForumAnswer: postForumAnswer,
+  postForumRating: postForumRating
 };
