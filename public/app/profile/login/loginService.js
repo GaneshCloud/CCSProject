767136dc1495d5c1b4068b6f9271ffae00826810
januNithi <csejananim@gmail.com>
2016-05-31 16:49:43 +0530

    Updated with code style changes

--- public/app/profile/login/loginService.js
+++ public/app/profile/login/loginService.js
@@ -3,69 +3,69 @@
  */
 angular.module('myApp')
     .factory('loginService', function($http, $window, $q) {
-        var httpPromise;
-        return {
-            
-            loginWithFacebook: function () {
-                $window.location.href = '/auth/facebook';
-            },
+      var httpPromise;
+      return {
 
-            loginWithGoogle: function () {
-                $window.location.href = '/auth/google';
-            },
+        loginWithFacebook: function() {
+          $window.location.href = '/auth/facebook';
+        },
 
-            verifyUser: function (user, password) {
-                var dfr = $q.defer();
+        loginWithGoogle: function() {
+          $window.location.href = '/auth/google';
+        },
 
-                httpPromise = $http({
-                    method: 'post',
+        verifyUser: function(user, password) {
+          var dfr = $q.defer();
 
-                    url: '/auth/verifyUser',
+          httpPromise = $http({
+            method: 'post',
 
-                    data: {
-                        user: user,
+            url: '/auth/verifyUser',
 
-                        password: password
+            data: {
+              user: user,
 
-                    }
+              password: password
 
-                });
-
-                httpPromise.then(function (response) {
-                    dfr.resolve(response.data);
-                }, function (error) {
-                    console.error(error);
-                });
-
-                return dfr.promise;
-            },
-
-            profilePage: function () {
-                var dfr = $q.defer();
-
-                httpPromise = $http({
-                    method: 'get',
-
-                    url: '/connect/getPersonalData'
-
-                });
-
-                httpPromise.then(function (response) {
-                    dfr.resolve(response);
-
-                    if (response.data.userType === 'admin') {
-                        $window.location.href = '/profile/adminDashboard';
-                    } else if(response.data.userType === 'user') {
-                        $window.location.href = '/profile/userDashboard';
-                    }else {
-                        $window.location.href = '/';
-                    }
-                }, function (error) {
-                    console.error(error);
-                });
-
-                return dfr.promise;
             }
 
+          });
+
+          httpPromise.then(function(response) {
+            dfr.resolve(response.data);
+          }, function(error) {
+            console.error(error);
+          });
+
+          return dfr.promise;
+        },
+
+        profilePage: function() {
+          var dfr = $q.defer();
+
+          httpPromise = $http({
+            method: 'get',
+
+            url: '/connect/getPersonalData'
+
+          });
+
+          httpPromise.then(function(response) {
+            dfr.resolve(response);
+
+            if (response.data.userType === 'admin') {
+              $window.location.href = '/profile/adminDashboard';
+            } else if (response.data.userType === 'user') {
+              $window.location.href = '/profile/userDashboard';
+            }else {
+              $window.location.href = '/';
+            }
+          }, function(error) {
+            console.error(error);
+          });
+
+          return dfr.promise;
         }
+
+      };
     });
