28aa3428a379af664d5742bb1401f9e38a951d41
januNithi <csejananim@gmail.com>
2016-05-31 16:48:15 +0530

    Updated with code style changes

--- public/app/profile/changePassword/changePasswordController.js
+++ public/app/profile/changePassword/changePasswordController.js
@@ -4,156 +4,156 @@
 
 
 (function() {
-    function changePasswordController($scope,$window,changePasswordService,ngProgressFactory,spinnerService,userDashboardService) {
+  function changePasswordController($scope,$window,changePasswordService,ngProgressFactory,spinnerService,userDashboardService) {
 
-        $scope.progressbar = ngProgressFactory.createInstance();
+    $scope.progressbar = ngProgressFactory.createInstance();
 
-        $scope.userCredentials = false;
+    $scope.userCredentials = false;
 
-        userDashboardService.checkUser();
+    userDashboardService.checkUser();
 
-        $scope.isOldPasswordValid = function(oldPassword) {
+    $scope.isOldPasswordValid = function(oldPassword) {
 
-            if (oldPassword.$dirty) {
+      if (oldPassword.$dirty) {
 
-                if (oldPassword.$viewValue === $scope.personalData.password) {
+        if (oldPassword.$viewValue === $scope.personalData.password) {
 
-                    $scope.isPasswordEditFormValid = true;
+          $scope.isPasswordEditFormValid = true;
 
-                    return false;
+          return false;
 
-                } else {
+        } else {
 
-                    $scope.isPasswordEditFormValid = false;
+          $scope.isPasswordEditFormValid = false;
 
-                    return true;
+          return true;
 
-                }
+        }
 
-            }
-        };
+      }
+    };
 
-        $scope.isRePasswordValid = function(newPassword,reNewPassword) {
+    $scope.isRePasswordValid = function(newPassword,reNewPassword) {
 
-            if (reNewPassword.$dirty) {
+      if (reNewPassword.$dirty) {
 
-                if (newPassword.$viewValue == reNewPassword.$viewValue) {
+        if (newPassword.$viewValue == reNewPassword.$viewValue) {
 
-                    $scope.isPasswordEditFormValid = true;
+          $scope.isPasswordEditFormValid = true;
 
-                    return false;
+          return false;
 
-                }else {
+        }else {
 
-                    $scope.isPasswordEditFormValid = false;
+          $scope.isPasswordEditFormValid = false;
 
-                    return true;
+          return true;
 
-                }
+        }
 
-            }
+      }
 
-        };
+    };
 
-        $scope.isPasswordValid = function(newPassword) {
-            return newPassword.$valid;
-        };
+    $scope.isPasswordValid = function(newPassword) {
+      return newPassword.$valid;
+    };
 
-        $scope.isPasswordInvalid = function(newPassword) {
-            return newPassword.$dirty && newPassword.$invalid;
-        };
+    $scope.isPasswordInvalid = function(newPassword) {
+      return newPassword.$dirty && newPassword.$invalid;
+    };
 
-        $scope.updatePassword = function(newPassword) {
+    $scope.updatePassword = function(newPassword) {
 
-            if ($window.confirm('Are You Sure ! Do you need to update the changes?')) {
+      if ($window.confirm('Are You Sure ! Do you need to update the changes?')) {
 
-                $scope.personalData.password = newPassword.$viewValue;
+        $scope.personalData.password = newPassword.$viewValue;
 
-                $scope.progressbar.start();
-                spinnerService.show('html5spinner');
+        $scope.progressbar.start();
+        spinnerService.show('html5spinner');
 
-                changePasswordService.updatePersonalData($scope.personalData).then(function() {
-                    $scope.progressbar.complete();
-                    spinnerService.hide('html5spinner');
-                    angular.element('#result').html('<div class="alert alert-success"><button type="button" class="close">×</button>Password Changed!</div>');
-                    $window.setTimeout(function() {
-                        $('.alert').fadeTo(500, 0).slideUp(500, function() {
-                            $(this).remove();
-                        });
-                    }, 5000);
-                    $('.alert .close').on('click', function(e) {
-                        console.log('e' + e);
-                        $(this).parent().fadeTo(500, 0).slideUp(500);
-                    });
+        changePasswordService.updatePersonalData($scope.personalData).then(function() {
+          $scope.progressbar.complete();
+          spinnerService.hide('html5spinner');
+          angular.element('#result').html('<div class="alert alert-success"><button type="button" class="close">×</button>Password Changed!</div>');
+          $window.setTimeout(function() {
+            $('.alert').fadeTo(500, 0).slideUp(500, function() {
+              $(this).remove();
+            });
+          }, 5000);
+          $('.alert .close').on('click', function(e) {
+            console.log('e' + e);
+            $(this).parent().fadeTo(500, 0).slideUp(500);
+          });
 
-                    $timeout(changePasswordService.profilePage(),6000);
+          $timeout(changePasswordService.profilePage(),6000);
 
-                });
+        });
 
-            } else {
-                // $scope.progressbar.start();
-                spinnerService.show('html5spinner');
-                changePasswordService.profilePage().then(function() {
-                    // $scope.progressbar.complete();
-                    spinnerService.hide('html5spinner');
-                });
+      } else {
+        // $scope.progressbar.start();
+        spinnerService.show('html5spinner');
+        changePasswordService.profilePage().then(function() {
+          // $scope.progressbar.complete();
+          spinnerService.hide('html5spinner');
+        });
 
-            }
+      }
 
-        };
+    };
 
-        $scope.onCancelChangePassword = function() {
+    $scope.onCancelChangePassword = function() {
 
-            if ($window.confirm('Are You Sure ! Do you need to leave the changes?')) {
-                // $scope.progressbar.start();
-                spinnerService.show('html5spinner');
-                changePasswordService.profilePage().then(function() {
-                    // $scope.progressbar.complete();
-                    spinnerService.hide('html5spinner');
-                });
+      if ($window.confirm('Are You Sure ! Do you need to leave the changes?')) {
+        // $scope.progressbar.start();
+        spinnerService.show('html5spinner');
+        changePasswordService.profilePage().then(function() {
+          // $scope.progressbar.complete();
+          spinnerService.hide('html5spinner');
+        });
 
-            }
+      }
 
-        };
+    };
 
-        var getPersonalData = function() {
+    var getPersonalData = function() {
 
-            changePasswordService.getPersonalData()
+      changePasswordService.getPersonalData()
 
                 .then(function(response) {
 
-                    $scope.personalData = response.data;
+                  $scope.personalData = response.data;
 
-                    if ($scope.personalData.userType === 'admin') {
-                        $scope.userCredentials = false;
-                    }
+                  if ($scope.personalData.userType === 'admin') {
+                    $scope.userCredentials = false;
+                  }
 
-                    if ($scope.personalData.facebook_img !== null) {
+                  if ($scope.personalData.facebook_img !== null) {
 
-                        $scope.file = '../images/uploads/' + $scope.personalData.facebook_img;
+                    $scope.file = '../images/uploads/' + $scope.personalData.facebook_img;
 
-                        console.log('User Profile Pic --->' + $scope.file);
+                    console.log('User Profile Pic --->' + $scope.file);
 
-                    }
+                  }
 
-                    if ($scope.personalData.facebook_img !== null) {
-                        $scope.fb_link = false;
-                    }
-                    if ($scope.personalData.google_img !== null) {
-                        $scope.google_link = false;
-                    }
+                  if ($scope.personalData.facebook_img !== null) {
+                    $scope.fb_link = false;
+                  }
+                  if ($scope.personalData.google_img !== null) {
+                    $scope.google_link = false;
+                  }
 
                 }, function(error) {
 
-                    console.error(error);
+                  console.error(error);
 
                 });
 
-        };
+    };
 
-        getPersonalData();
+    getPersonalData();
 
-    }
+  }
 
-    myApp.controller('changePasswordController',changePasswordController);
+  myApp.controller('changePasswordController',changePasswordController);
 })();
