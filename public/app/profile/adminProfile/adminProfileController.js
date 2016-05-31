f5c0c55704f6375f63b4621a1a349398e4efbcb6
januNithi <csejananim@gmail.com>
2016-05-31 16:47:18 +0530

    Updated with code style changes

--- public/app/profile/adminProfile/adminProfileController.js
+++ public/app/profile/adminProfile/adminProfileController.js
@@ -4,144 +4,144 @@
 
 (function() {
 
-    function adminProfileController($scope,$window,adminProfileService,
-                                    ngProgressFactory,spinnerService,filterFilter,adminDashboardService) {
-        
-        adminDashboardService.checkAdmin();
+  function adminProfileController($scope,$window,adminProfileService,
+                                  ngProgressFactory,spinnerService,filterFilter,adminDashboardService) {
 
-        $scope.progressbar = ngProgressFactory.createInstance();
+    adminDashboardService.checkAdmin();
 
-        $scope.userCredentials = false;
+    $scope.progressbar = ngProgressFactory.createInstance();
 
-        $scope.file = '../images/profile/no_profile.png';
+    $scope.userCredentials = false;
 
-        $scope.noData = true;
+    $scope.file = '../images/profile/no_profile.png';
 
-        $scope.cur_page = 1;
+    $scope.noData = true;
 
-        $scope.userDetails = [];
+    $scope.cur_page = 1;
 
-        $scope.userDetailsFilter = [];
+    $scope.userDetails = [];
 
-        $scope.items_per_page = 6;
+    $scope.userDetailsFilter = [];
 
-        $scope.totalLength = 0;
+    $scope.items_per_page = 6;
 
-        $scope.max_size = Math.ceil($scope.userDetails / $scope.items_per_page);
+    $scope.totalLength = 0;
 
-        $scope.search = [];
+    $scope.max_size = Math.ceil($scope.userDetails / $scope.items_per_page);
 
-        $scope.showPagination = function() {
-            return $scope.noData;
-        };
+    $scope.search = [];
 
-        var getUserDetails = function() {
+    $scope.showPagination = function() {
+      return $scope.noData;
+    };
 
-            adminProfileService.getUserDetails()
+    var getUserDetails = function() {
+
+      adminProfileService.getUserDetails()
 
                 .then(function(response) {
 
-                    $scope.userDetails = response.data;
+                  $scope.userDetails = response.data;
 
-                    if ($scope.userDetails.length > 0) {
-                        $scope.noData = false;
+                  if ($scope.userDetails.length > 0) {
+                    $scope.noData = false;
 
-                        $scope.totalLength = $scope.userDetails.length;
+                    $scope.totalLength = $scope.userDetails.length;
 
-                        $scope.max_size = Math.ceil($scope.totalLength / $scope.items_per_page);
+                    $scope.max_size = Math.ceil($scope.totalLength / $scope.items_per_page);
 
-                        $scope.$watch('cur_page + items_per_page', function() {
+                    $scope.$watch('cur_page + items_per_page', function() {
 
-                            var begin = (($scope.cur_page - 1) * $scope.items_per_page), end = begin + $scope.items_per_page;
-                            console.log(begin + ' ' + end);
-                            $scope.userDetailsFilter = $scope.userDetails.slice(begin, end);
-                            // Alert("data"+$scope.searchres);
-                        });
-                    }
+                      var begin = (($scope.cur_page - 1) * $scope.items_per_page), end = begin + $scope.items_per_page;
+                      console.log(begin + ' ' + end);
+                      $scope.userDetailsFilter = $scope.userDetails.slice(begin, end);
+                      // Alert("data"+$scope.searchres);
+                    });
+                  }
 
                 }, function(error) {
 
-                    console.error(error);
+                  console.error(error);
 
                 }) ;
 
-        };
+    };
 
-        $scope.goToDashboard = function() {
-            // $scope.progressbar.start();
-            spinnerService.show('html5spinner');
-            adminProfileService.goToAdminDashboard().then(function() {
-                // $scope.progressbar.complete();
-                spinnerService.hide('html5spinner');
-            });
-        };
+    $scope.goToDashboard = function() {
+      // $scope.progressbar.start();
+      spinnerService.show('html5spinner');
+      adminProfileService.goToAdminDashboard().then(function() {
+        // $scope.progressbar.complete();
+        spinnerService.hide('html5spinner');
+      });
+    };
 
-        $scope.getImageSrcProfile = function(user) {
+    $scope.getImageSrcProfile = function(user) {
 
-            var img;
+      var img;
 
-            if (user.profile_pic) {
-                img = '../uploads/profile/' + user.profile_pic;
-            }else if (user.facebook_img) {
-                img = '../uploads/profile/' + user.facebook_img;
-            }else if (user.google_img) {
-                img = '../uploads/profile/' + user.google_img;
-            }else {
-                img = $scope.file;
-            }
+      if (user.profile_pic) {
+        img = '../uploads/profile/' + user.profile_pic;
+      }else if (user.facebook_img) {
+        img = '../uploads/profile/' + user.facebook_img;
+      }else if (user.google_img) {
+        img = '../uploads/profile/' + user.google_img;
+      }else {
+        img = $scope.file;
+      }
 
-            return img;
+      return img;
 
-        };
+    };
 
-        $scope.getImageSrc = function(image) {
+    $scope.getImageSrc = function(image) {
 
-            var img;
+      var img;
 
-            if (image) {
-                img = '../uploads/profile/' + image;
-            }else {
-                img = $scope.file;
-            }
-            return img;
-        };
+      if (image) {
+        img = '../uploads/profile/' + image;
+      }else {
+        img = $scope.file;
+      }
+      return img;
+    };
 
-        $scope.onLogout = function() {
+    $scope.onLogout = function() {
 
-            if ($window.confirm('Are You Sure ! Do you need to Log Out?')) {
-                // $scope.progressbar.start();
-                spinnerService.show('html5spinner');
-                adminProfileService.logout().then(function() {
-                    // $scope.progressbar.complete();
-                    spinnerService.hide('html5spinner');
-                });
+      if ($window.confirm('Are You Sure ! Do you need to Log Out?')) {
+        // $scope.progressbar.start();
+        spinnerService.show('html5spinner');
+        adminProfileService.logout().then(function() {
+          // $scope.progressbar.complete();
+          spinnerService.hide('html5spinner');
+        });
 
-            }
+      }
 
-        };
+    };
 
-        $scope.resetFilters = function() {
-            // Needs to be a function or it won't trigger a $watch
-            $scope.search = [];
-        };
+    $scope.resetFilters = function() {
+      // Needs to be a function or it won't trigger a $watch
+      $scope.search = [];
+    };
 
-        // $watch search to update pagination
-        $scope.$watch('search', function(newVal, oldVal) {
-            console.log('oldVal' + oldVal);
-            $scope.userDetailsFilters = filterFilter($scope.userDetails, newVal);
-            $scope.totalLength = $scope.userDetailsFilters.length;
-            $scope.max_size = Math.ceil($scope.totalLength / $scope.items_per_page);
-            $scope.cur_page = 1;
-            var begin = (($scope.cur_page - 1) * $scope.items_per_page), end = begin + $scope.items_per_page;
-            console.log(begin + ' ' + end);
-            $scope.userDetailsFilter = $scope.userDetailsFilters.slice(begin, end);
-        }, true);
+    // $watch search to update pagination
+    $scope.$watch('search', function(newVal, oldVal) {
+      console.log('oldVal' + oldVal);
+      $scope.userDetailsFilters = filterFilter($scope.userDetails, newVal);
+      $scope.totalLength = $scope.userDetailsFilters.length;
+      $scope.max_size = Math.ceil($scope.totalLength / $scope.items_per_page);
+      $scope.cur_page = 1;
+      var begin = (($scope.cur_page - 1) * $scope.items_per_page), end = begin + $scope.items_per_page;
+      console.log(begin + ' ' + end);
+      $scope.userDetailsFilter = $scope.userDetailsFilters.slice(begin, end);
+    }, true);
 
-        getUserDetails();
+    getUserDetails();
 
 
-    }
+  }
 
-    myApp.controller('adminProfileController',adminProfileController);
+  myApp.controller('adminProfileController',adminProfileController);
 
 })();
