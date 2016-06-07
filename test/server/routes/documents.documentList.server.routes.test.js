c91b937206da26a9d2a313579c043c33367948f7
ppyoosuf <pp.yoosuf@gmail.com>
2016-06-07 16:59:43 -0700

    karma supporting modules

---
+++ test/server/routes/documents.documentList.server.routes.test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 6/6/2016.
+ */
+
+// var expect  = require("chai").expect;
+// var sinon   = require("sinon");
+// var Document = require("../../../routes/documents.documentList.server.routes");
+//
+// describe("documentList",function(){
+//     it('should call save once', function() {
+//         var save = sinon.spy(documentListCtrl, 'searchDocument');
+//
+//        // setupNewUser({ name: 'test' }, function() { });
+//
+//         searchDocument.restore();
+//         sinon.assert.calledOnce(searchDocument);
+//     });
+// });
