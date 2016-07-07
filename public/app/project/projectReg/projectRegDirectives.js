4396cb3d91320c30cc6e7eeb61803ad454187944
iosdevelopment <sasiuar017@gmail.com>
2016-07-07 10:27:56 +0530

    test cases

--- public/app/project/projectReg/projectRegDirectives.js
+++ public/app/project/projectReg/projectRegDirectives.js
@@ -18,9 +18,6 @@
                 template: '<ul class="dropdown-menu" role="menu"><li><a ng-click="orderAsc()"><span class="glyphicon glyphicon-sort-by-alphabet"></span>&nbsp;Ascending</a></li><li><a ng-click="orderDsc()"><span class="glyphicon glyphicon-sort-by-alphabet-alt"></span>&nbsp;Descending</a></li><li><a ng-click="searchBox()" ng-show="visibleArr"><span class="glyphicon glyphicon-search"></span>&nbsp;Search</a></li><li><a ng-click="search1Box()" ng-hide="visibleArr"><span class="glyphicon glyphicon-search"></span>&nbsp;Hide search</a></li></ul>',
                 link:function(scope,elem,attr){
 
-                },
-                controller:function ($scope) {
-                    alert("ssssssss"+$scope.field);
                 }
 
             };
