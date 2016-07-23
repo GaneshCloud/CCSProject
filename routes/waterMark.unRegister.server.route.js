fea1a9d4834e3234ebd040978601d3aa08d79ba9
januNithi <csejananim@gmail.com>
2016-07-23 18:27:40 +0530

    Updated Restriction to dashboard view of unregistered Users.

--- routes/waterMark.unRegister.server.route.js
+++ routes/waterMark.unRegister.server.route.js
@@ -2,5 +2,5 @@ module.exports=function(app){
     
     var con=require('../controllers/waterMark.unRegister.server.controller.js');
     
-    app.get('/fileDoc',con.getAllDoc);
+    // app.get('/fileDoc',con.getAllDoc);
 };
